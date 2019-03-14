48e795fc51509075c750a4ead9a44f4c645c6e1e
Sheila Mundo <sheilamundo@MacBook-Pro-de-Sheila.local>
2019-03-14 22:22:33 +0100

    change welcome

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,8 +5,6 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
-
 
 class DefaultController extends Controller
 {
@@ -15,9 +13,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-
-        return new Response(
-            '<html><body>Bienvenidos al Proyecto de Sheila! <a href="/login">Inicia la sesion</a></body></html>'
-        );
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
     }
 }
